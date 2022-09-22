157c8f838d212bb3589ae13cb9810cd8ef1c4127
BeardR6S <brandon.allan.hale@gmail.com>
2022-09-22 13:59:01 -0400

    Stepping away, updating so I don't lose progress

--- Single Page Programs/CYOA.py
+++ Single Page Programs/CYOA.py
@@ -3,19 +3,20 @@
 name = input("Type your name: ")
 print(f"Welcome {name} to this adventure!")
 
-q1 = input("You awake in a bed, you can either get up or go back to sleep, type \"get up\" to get up and \"sleep\" to go back to sleep: ").lower()
+answer = input("You awake in a bed, you can either get up or go back to sleep, type \"get up\" to get up and \"sleep\" to go back to sleep: ").lower()
 
-if q1 == "sleep":
-    q1 = input("You chose to sleep through the day, therefor missing out on the days adventure, you lose.")
+if answer == "sleep":
+    answer = input("You chose to sleep through the day, therefor missing out on the days adventure, you lose.")
 
-if q1 == "get up":
-    q1 = input("You chose to get out of bed, you can either eat breakfast while watching scrolling through TikTok, or take a shower first then eat breakfast. Type \"eat\" to eat first, or \"shower\" to take a shower first: ").lower()
+elif answer == "get up":
+    answer = input("You chose to get out of bed, you can either eat breakfast while watching scrolling through TikTok, or take a shower first then eat breakfast. Type \"eat\" to eat first, or \"shower\" to take a shower first: ").lower()
         
-    if q1 == "eat":
+    if answer == "eat":
         print("You chose to eat first while scrolling through TikTok, however while doing so you lost track of time and end up just staying home all day. You lose the game.")
-    
-    elif q1 == "shower":
-        print("You decided to take your shower first then eat, which gives you a refreshing start to your day.")
-    
+    elif answer == "shower":
+        print("You decided to take your shower first then eat. After you finish eating you look at your phone and realize its time to head to work, do you walk or drive to work? (walk/drive)")
+        if answer == "walk":
+            answer = input("You decide to walk to work, you grab your keys and head out the door. While walking to work, you pass by a coffee shop, do you stop and get some coffee? (yes/no): ")
+        
     else:
         print("Invalid input, you lose.")
